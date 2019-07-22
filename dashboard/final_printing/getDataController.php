7746543e24c2b0def475f29c14d644d14de5fa51
subramaniks <48723435+subramaniks@users.noreply.github.com>
2019-07-22 13:26:19 +0530

    Update getDataController.php

--- dashboard/final_printing/getDataController.php
+++ dashboard/final_printing/getDataController.php
@@ -93,10 +93,11 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 
     $wrk_ctr_code=$_POST['wrk_ctr_code'];
         
-           $last_dept ="SELECT a.wrk_ctr_code,tb_t_job_card_trans.batch_no,tb_m_jobcard.fg_code,tb_m_jobcard.cust_name,tb_m_jobcard.Siliconize,tb_m_jobcard.true_pass,tb_m_jobcard.plan,tb_m_jobcard.req_date,if(tb_m_jobcard.urgent = 0,'REGULAR','URGENT')as type,a.qty as ok_qty,ifnull(sum(b.qty),0)as reject_qty,  date_format(tb_t_job_card_trans.updated_at,'%d-%m-%Y %H:%i:%s')as updated_at FROM `tb_t_job_card_trans`
-            join tb_m_jobcard on tb_t_job_card_trans.batch_no = tb_m_jobcard.batch_no
-            join(SELECT ph.mach_code, ph.batch_no, pi.qty, ph.emp_id, ph.created_at,ph.wrk_ctr_code FROM `tb_t_prod_h` ph JOIN tb_t_prod_i pi ON pi.batch_no=ph.batch_no AND pi.sl_no=ph.sl_no AND ph.qlty_type_code=500 ) a on a.batch_no = tb_t_job_card_trans.batch_no and     tb_t_job_card_trans.present_mach=a.mach_code
-            left OUTER join(SELECT ph.mach_code, ph.batch_no, pi.qty, ph.emp_id, ph.created_at,ph.wrk_ctr_code FROM `tb_t_prod_h` ph JOIN tb_t_prod_i pi ON pi.batch_no=ph.batch_no AND pi.sl_no=ph.sl_no AND ph.qlty_type_code=502 ) b on b.batch_no = tb_t_job_card_trans.batch_no and tb_t_job_card_trans.present_mach=b.mach_code
+           $last_dept ="SELECT a.wrk_ctr_code,tb_t_job_card_trans.batch_no,tb_m_jobcard.fg_code,tb_m_fg.series,tb_m_jobcard.cust_name,tb_m_jobcard.Siliconize,tb_m_jobcard.true_pass,tb_m_jobcard.plan,tb_m_jobcard.req_date,if(tb_m_jobcard.urgent = 0,'REGULAR','URGENT')as type,a.qty as ok_qty,ifnull(sum(b.qty),0)as reject_qty,  date_format(tb_t_job_card_trans.updated_at,'%d-%m-%Y %H:%i:%s')as updated_at FROM `tb_t_job_card_trans`
+           join tb_m_jobcard on tb_t_job_card_trans.batch_no = tb_m_jobcard.batch_no
+           join(SELECT ph.mach_code, ph.batch_no, pi.qty, ph.emp_id, ph.created_at,ph.wrk_ctr_code FROM `tb_t_prod_h` ph JOIN tb_t_prod_i pi ON pi.batch_no=ph.batch_no AND pi.sl_no=ph.sl_no AND ph.qlty_type_code=500 ) a on a.batch_no = tb_t_job_card_trans.batch_no and     tb_t_job_card_trans.present_mach=a.mach_code
+           left OUTER join(SELECT ph.mach_code, ph.batch_no, pi.qty, ph.emp_id, ph.created_at,ph.wrk_ctr_code FROM `tb_t_prod_h` ph JOIN tb_t_prod_i pi ON pi.batch_no=ph.batch_no AND pi.sl_no=ph.sl_no AND ph.qlty_type_code=502 ) b on b.batch_no = tb_t_job_card_trans.batch_no and tb_t_job_card_trans.present_mach=b.mach_code
+           join tb_m_fg on tb_m_jobcard.fg_code = tb_m_fg.fg_code
             where present_dept = '$wrk_ctr_code' and status_code = 803 and oper_status = 807 GROUP by batch_no order by tb_t_job_card_trans.updated_at DESC";
 
             $last_dept_item=mysqli_query($con,$last_dept) or die('Error:'.mysqli_error($con));
@@ -104,6 +105,7 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
                 $wrk_ctr_code=$row['wrk_ctr_code'];
                 $batch_no=$row['batch_no'];
                 $fg_code=$row['fg_code'];
+                $series=$row['series'];
                 $cust_name=$row['cust_name'];
                 $plan=$row['plan'];
                 $req_date=$row['req_date'];
@@ -117,6 +119,7 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
                 $last_dept_details[]=array('wrk_ctr_code' =>$wrk_ctr_code,
                 'batch_no' =>$batch_no,
                 'fg_code' =>$fg_code,
+                'series' =>$series,
                 'cust_name' =>$cust_name,
                 'plan' =>$plan,
                 'req_date' =>$req_date,
