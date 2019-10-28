15712ba55ae21b5a7b6cea3b47bbe34d65e672ea
SubrahmanyaBhatA <subrahmanyab@eimsolutions.com>
2019-10-28 13:21:56 +0530

    commited by subramani

--- dashboard/production_dashboard/getDataController.php
+++ dashboard/production_dashboard/getDataController.php
@@ -139,19 +139,20 @@ if(isset($data['loadData'])){
 
     $final_date=$data['final_date'];
 
-    $sql="select date_,monthly_man_hours from tb_t_prod_dash_i where date_='".$final_date."'";
+    $sql="select date_,monthly_man_hours,last_month_man_hours from tb_t_prod_dash_i where date_='".$final_date."'";
 
     $sqlRes=mysqli_query($con,$sql) or die('Error:'.mysqli_error($con));
     while ($row=mysqli_fetch_array($sqlRes)){
         $date_=$row['date_']; 
         $monthly_man_hours=$row['monthly_man_hours'];
+        $last_month_man_hours=$row['last_month_man_hours'];
     }
 
 
     fillActualData($final_date);
     fillStockData($final_date);
     monthlyProduction($final_date,$monthly_man_hours);
-
+    lastMonthProduction($final_date,$last_month_man_hours);
 
 }
 
@@ -277,6 +278,36 @@ function monthlyProduction($date,$monthly_man_hoursDB){
     );
 	
 	//print_r($DataMarge_prod_dash_i);
+
+    $cond=' date_ ="'.$date.'"';
+    $update_sql = mysqli_update_array($table_prod_dash_i,$DataMarge_prod_dash_i,"submit",$cond);
+    $result1=mysqli_query($con,$update_sql); 
+
+}
+
+function lastMonthProduction($date,$last_month_man_hoursDB){
+
+    require '../common/db.php';
+
+    //Total Input Qty
+    $sql="select SUM(pi.qty) as output_qty,SUM(jb.ord_qty) as input_qty from tb_t_prod_i pi join tb_m_jobcard jb on jb.batch_no = pi.batch_no where pi.qlty_code = 620 and pi.wrk_ctr_code = 103021 and YEAR(pi.updated_at) = YEAR('".$date."' - INTERVAL 1 MONTH) AND MONTH(pi.updated_at) = MONTH('".$date."' - INTERVAL 1 MONTH)";
+
+    $sqlRes=mysqli_query($con,$sql) or die('Error:'.mysqli_error($con));
+    while ($row=mysqli_fetch_array($sqlRes)){
+        $totalInputQty=$row['input_qty']; 
+        $totalOkQty=$row['output_qty'];
+    }
+
+  
+    //$last_month_production = round((( $totalOkQty / $totalInputQty) * 100), 2);  
+    $last_month_production = ($totalOkQty/12);  
+
+    $table_prod_dash_i = 'tb_t_prod_dash_i';
+    $DataMarge_prod_dash_i=array(           
+        'last_month_production'=>round($last_month_production,3),
+        'last_month_yield'=>round((( $totalOkQty / $totalInputQty) * 100), 3) ,
+        'last_month_productivity'=>round( (($totalOkQty/12) / $last_month_man_hoursDB), 2)
+    );
 
     $cond=' date_ ="'.$date.'"';
     $update_sql = mysqli_update_array($table_prod_dash_i,$DataMarge_prod_dash_i,"submit",$cond);
